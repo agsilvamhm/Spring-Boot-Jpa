70e2e80c6fee7c74f2750fabccb6d41a29503aac
Adalberto Gonçalves da Silva <adalberto.goncalves.silva@gmail.com>
2022-10-21 20:23:12 -0300

    Primeiro comit do projeto

---
+++ src/main/java/com/codegate/Aula02/Emprego.java
@@ -0,0 +1,43 @@
+package com.codegate.Aula02;
+
+import javax.persistence.Entity;
+
+@Entity
+public class Emprego {
+    private long id;
+    private String titulo;
+    private String empresa;
+    private String descricao;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
